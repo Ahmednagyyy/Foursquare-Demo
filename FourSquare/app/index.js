df3698198472b42fa81889b0b38df81b86fcacca
Ahmednagii <ahmed.nagi@extremesolution.com>
2020-06-13 23:07:08 +0200

    add app auth for 3rd party authorization

--- FourSquare/app/index.js
+++ FourSquare/app/index.js
@@ -3,13 +3,19 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { StoreProvider } from 'easy-peasy';
 import { Actions, Router, Scene, Modal } from 'react-native-router-flux';
 import { ThemeProvider } from 'react-native-elements';
-import { Platform, View, Text } from 'react-native';
+import { Platform, View, Text, TouchableOpacity, AsyncStorage, Alert } from 'react-native';
+import { authorize, revoke } from 'react-native-app-auth';
+import Config from "react-native-config";
+import axios from 'axios';
+
 
 export default class App extends Component {
+
+
   render() {
     return (
-      <View>
-        <Text>Hello</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                  <Text>Foursquare Demo</Text>
       </View>
     );
   }
